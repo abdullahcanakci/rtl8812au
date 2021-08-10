fe9682ba986c67033321f4eafdaf47dda70eff90
Abdullah Çanakçı <abdullahcanakci.0@gmail.com>
2021-08-10 22:35:23 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Editor } from "./components";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Editor/Editor.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Editor.scss";
+
+function Editor() {
+  return (
+    <div className="editor">
+      <header className="">Editor</header>
+    </div>
+  );
+}
+
+export default Editor;
